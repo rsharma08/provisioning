a5c462c770226061471878ac9dfc0ac345e62308
Raman Sharma <raman.sharma@msn.com>
2022-05-10 15:29:05 +0000

    add

--- variable.tf
+++ variable.tf
@@ -7,3 +7,31 @@ variable "instancetype" {
     prd = "t2.large"
   }
 }
+
+variable "chef_server_url" {
+    description = "Chef Server URL"
+    default = "https://api.chef.io/organizations/rsharma"
+}
+
+variable "chef_user_key" {
+    description = "Path to Chef user .pem"
+    default = "./chef.pem"
+}
+
+variable "chef_user_name" {
+    description = "Name of Chef user"
+    default = "rsharma"
+}
+
+variable "chef_node_name" {
+    description = "Name for node instance"
+    default = "test.com"
+}
+
+variable "chef_policy_group" {
+    description = "Policy Group for the node"
+}
+
+variable "chef_policy_name" {
+    description = "Policy Name for the node"
+}
